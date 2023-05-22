729b8e36f9a384cbeb3f933dd0c5acfbd4e036da
Benewende P. BONKOUNGOU <Benewende.b@yahoo.com>
2023-05-22 03:06:36 -0700

    I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Return: 0 if successful
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
