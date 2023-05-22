243b8c65e117689d694452f359c1b216dde79f57
Benewende P. BONKOUNGOU <Benewende.b@yahoo.com>
2023-05-22 03:59:48 -0700

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
-/**
-* main - Entry point of the program
-*
-* Return: 0 if successful
-*/
 int main(void)
 {
-int i, j;
+int i;
 for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j < 10; j++)
-{
 putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9)
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
